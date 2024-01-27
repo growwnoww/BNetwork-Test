7c0d31ad9da4472054a89ed94c50b8932cfbcc72
pratik bade <pratikmbade18@gmail.com>
2024-01-27 06:06:36 +0530

    Producation Issues

--- components/clientcomponents/bnsystemClientComp/ClubBStructure.tsx
+++ components/clientcomponents/bnsystemClientComp/ClubBStructure.tsx
@@ -26,10 +26,10 @@ const ClubBStructure = ({centerImg, ornageImg,sil1Img,sil2Img,redImg,green1Img,g
                     fill="#1A1A1A"></rect>
                 <path
                     d="M187.081 111.174C227.879 111.174 267.007 120.53 295.856 137.185C324.705 153.839 340.912 176.427 340.912 199.98L283.26 199.98C283.26 185.254 273.127 171.132 255.09 160.719C237.053 150.306 212.589 144.456 187.081 144.456L187.081 111.174Z"
-                    fill="white" fill-opacity="0.1" stroke="#1A1A1A" stroke-linejoin="round"></path>
+                    fill="white" fillOpacity="0.1" stroke="#1A1A1A" stroke-linejoin="round"></path>
                 <path
                     d="M187.081 288.787C146.282 288.787 107.154 279.431 78.3054 262.776C49.4564 246.122 33.2492 223.534 33.2492 199.98L90.9011 199.98C90.9011 214.706 101.034 228.829 119.071 239.242C137.109 249.655 161.572 255.505 187.081 255.505L187.081 288.787Z"
-                    fill="white" fill-opacity="0.1" stroke="#1A1A1A" stroke-linejoin="round"></path>
+                    fill="white" fillOpacity="0.1" stroke="#1A1A1A" stroke-linejoin="round"></path>
                 <circle r="78.5"
                     transform="matrix(0.866044 0.499967 -0.866044 0.499967 187.081 199.981)"
                     stroke="#4D4D4D" stroke-linejoin="round"></circle>
--- components/dashboardComponents/LevelIcon.tsx
+++ components/dashboardComponents/LevelIcon.tsx
@@ -22,22 +22,22 @@ const LevelIcon = ({ level }: levelType) => {
         <rect width="56" height="56" rx="28" style={svgStyles}></rect>
                  <path
                     d="M28 2C32.564 2 37.0475 3.20136 41 5.48334C44.9525 7.76531 48.2347 11.0475 50.5167 15L47.1392 16.95C45.1995 13.5904 42.4096 10.8005 39.05 8.86084C35.6904 6.92116 31.8794 5.9 28 5.9V2Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
                   <path
                     d="M50.5167 15C52.7986 18.9525 54 23.436 54 28C54 32.5639 52.7986 37.0475 50.5167 41L47.1392 39.05C49.0788 35.6904 50.1 31.8794 50.1 28C50.1 24.1206 49.0788 20.3096 47.1392 16.95L50.5167 15Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
                   <path
                     d="M50.5167 41C48.2347 44.9525 44.9525 48.2347 41 50.5167C37.0475 52.7986 32.564 54 28 54L28 50.1C31.8794 50.1 35.6904 49.0788 39.05 47.1392C42.4096 45.1995 45.1995 42.4096 47.1392 39.05L50.5167 41Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
                   <path
                     d="M28 54C23.436 54 18.9525 52.7986 15 50.5167C11.0475 48.2347 7.76532 44.9525 5.48334 41L8.86084 39.05C10.8005 42.4096 13.5904 45.1995 16.95 47.1392C20.3096 49.0788 24.1206 50.1 28 50.1V54Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
                   <path
                     d="M5.48334 41C3.20136 37.0475 2 32.564 2 28C2 23.4361 3.20136 18.9525 5.48334 15L8.86084 16.95C6.92116 20.3096 5.9 24.1206 5.9 28C5.9 31.8794 6.92116 35.6904 8.86084 39.05L5.48334 41Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
                   <path
                     d="M5.48334 15C7.76532 11.0475 11.0475 7.76532 15 5.48334C18.9525 3.20136 23.436 2 28 2V5.9C24.1206 5.9 20.3096 6.92116 16.95 8.86084C13.5904 10.8005 10.8005 13.5904 8.86084 16.95L5.48334 15Z"
-                    fill={level == 1? highlightStyles.fill:'white'} fill-opacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
+                    fill={level == 1? highlightStyles.fill:'white'} fillOpacity={level == 1? highlightStyles.fillOpacity:'0.2'} stroke="black"></path>
       </svg>
       <p className="absolute ">{level}</p>
 
