8adc289eb3e8b76e7a4f3ccb98dc3ab6a3125211
pratik bade <pratikmbade18@gmail.com>
2024-06-03 21:19:43 +0530

    new-club-a-added

--- utils/SelectClubAData.ts
+++ utils/SelectClubAData.ts
@@ -8,53 +8,52 @@ export const SelectClubAData: SelectItemType[] = [
     {
         id: 1,
         value: "Earth",
-        label: "Earth 0.10$"
+        label: "Earth 10$"
     },
     {
         id: 2,
         value: "Moon",
-        label: "Moon 0.20$"
+        label: "Moon 25$"
     },
     {
         id: 3,
         value: "Mars",
-        label: "Mars 0.30$"
+        label: "Mars 50$"
     },
     {
         id: 4,
         value: "Mercury",
-        label: "Mercury 0.40$"
+        label: "Mercury 100$"
     },
     {
         id: 5,
         value: "Venus",
-        label: "Venus 0.50$"
+        label: "Venus 250$"
     },
     {
         id: 6,
         value: "Jupiter",
-        label: "Jupiter 0.60$"
+        label: "Jupiter 500$"
     },
     {
         id: 7,
         value: "Saturn",
-        label: "Saturn 0.70$"
+        label: "Saturn 1000$"
     },
     {
         id: 8,
         value: "Uranus",
-        label: "Uranus 0.80$"
+        label: "Uranus 2500$"
     },
     {
         id: 9,
         value: "Neptune",
-        label: "Neptune 0.90$"
+        label: "Neptune 5000$"
     },
     {
         id: 10,
         value: "Pluto",
-        label: "Pluto 1$"
+        label: "Pluto 10000$"
     }
-
 
 ]
